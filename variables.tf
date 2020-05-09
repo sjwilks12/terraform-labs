12fef528a3a10f9832bf91ea7dcf612bfc1a1270
Sam Wilkins <sawilkin@microsoft.com>
2020-05-08 19:44:29 -0500

    added web app

--- variables.tf
+++ variables.tf
@@ -1,6 +1,11 @@
 variable "loc" {
     description = "Default Azure region"
-    default     =   "West Europe"
+    default     =   "westeurope"
+}
+
+variable "webapplocs" {
+    description = "Web app locations"
+    default     =   [ "eastus2", "uksouth", "centralindia" ]
 }
 
 variable "tags" {
